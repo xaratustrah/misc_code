095bb9cbe0ccbab5f93f5222883ba10e2d4b2562
xaratustrah <shahab@zamail.de>
2015-01-07 14:32:46 +0100

    scape removed

--- python/query2list.py
+++ python/query2list.py
@@ -11,9 +11,8 @@ GSTORE = '/cvmfs/it.gsi.de/gstore/gstore64'
 
 def make_list(for_delete=False):
     data = sys.stdin.readlines()
-    flatlist = []
+    flat_list = []
     delete_list = []
-    out = []
     for line in data[1:]:
         count = 0
         index = line.find('STAGE') & line.find('CACHE') & line.find('TAPE') - 22
@@ -21,29 +20,28 @@ def make_list(for_delete=False):
             count += 1
         count += 1
         line = line[0:index - count]
-        fullpath = re.split('^[0-9]+:', line)[1:]
-        fullpath[0] = fullpath[0].replace(' ', '\ ')
+        full_path = re.split('^[0-9]+:', line)[1:]
+        # full_path[0] = full_path[0].replace(' ', '?')
 
-        flatlist.extend(fullpath)
-        separated = fullpath[0].split('/')
-        delete_list.append(GSTORE + " delete '" + separated[-1] + "' " + separated[1] + " '/" + '/'.join(
-            map(str, separated[2:-1])) + "'")
+        flat_list.extend(full_path)
+        separated_full_path = full_path[0].split('/')
+        delete_list.append(
+            GSTORE + " delete '" + separated_full_path[-1] + "' " + separated_full_path[1] + " '/" + '/'.join(
+                map(str, separated_full_path[2:-1])) + "'")
 
-    flatlist.sort()
+    flat_list.sort()
     delete_list.sort()
     if for_delete:
-        out = delete_list
+        print('%s' % '\n'.join(map(str, delete_list)))
     else:
-        out = flatlist
-
-    print('%s' % '\n'.join(map(str, out)))
+        print('%s' % '\n'.join(map(str, flat_list)))
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         make_list()
     if len(sys.argv) == 2:
-        if (sys.argv[1] == '-d'):
+        if sys.argv[1] == '-d':
             make_list(True)
         else:
             print('No arguments for simple sorted list or -d for sorted delete list.')
