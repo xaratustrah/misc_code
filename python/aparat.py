4101ff168cbdc0a1e384b645d67bf5a4d8c8c9e8
xaratustrah <shahab@zamail.de>
2015-02-20 00:17:14 +0100

    initial commit

---
+++ python/aparat.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""
+Downloads videos from Apart extracted from Emails. Save your emails as eml files. then use them either as
+command line arguments, or use pipe from stdin.
+
+python aparat.py *.eml
+cat *.eml | python aparat.py
+
+Feb 2015
+
+Xaratustrah
+
+"""
+import fileinput, re
+from bs4 import BeautifulSoup
+import urllib.request as ur
+
+
+def download_file(url, folder, name):
+    g = ur.urlopen(url)
+    with open('./{}/{}.mp4'.format(folder, name), 'b+w') as f:
+        f.write(g.read())
+    return
+
+
+if __name__ == '__main__':
+    cnt = 0
+    for line in fileinput.input():
+        # regexp from: http://stackoverflow.com/questions/6883049/regex-to-find-urls-in-string-in-python
+        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line)
+        if urls:
+            print('Downloading from: ' + urls[0])
+            page = ur.urlopen(urls[0])
+            soup = BeautifulSoup(page)
+            for tag in soup.findAll('a', href=True):
+                if tag['href'].endswith('.mp4'):
+                    file_url = (tag['href'])
+                    print('--> movie_{}.mp4'.format(cnt))
+                    download_file(file_url, '.', 'movie_{}'.format(cnt))
+                    cnt += 1
