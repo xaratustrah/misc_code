04afdad909023392dbf6d5f5288e5753b30cf65a
Xaratustrah <shahab.sanjari@gmail.com>
2017-06-01 01:34:05 +0200

    forgot to commit

---
+++ python/simp_serv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# simple server
+
+import BaseHTTPServer
+import CGIHTTPServer
+import cgitb
+
+cgitb.enable()  ## This line enables CGI error reporting
+
+server = BaseHTTPServer.HTTPServer
+handler = CGIHTTPServer.CGIHTTPRequestHandler
+server_address = ("", 8000)
+handler.cgi_directories = ["/"]
+
+httpd = server(server_address, handler)
+httpd.serve_forever()
